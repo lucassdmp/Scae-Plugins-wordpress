b2e770fabec573b9be44cdfaa24eb655002372a6
Lucas <lucassdmp01@gmail.com>
2022-12-21 08:20:28 -0300

    basic project setup

---
+++ scae_plugin.php
@@ -0,0 +1,11 @@
+<?php
+/*
+Plugin Name: Scae Custom Plugins
+Plugin URI: www.scae.academy
+Description: This plugin creates custom set of pages for SCAE site.
+Version: 1.0
+Author: João Lucas
+Author URI: https://github.com/lucassdmp
+*/
+
+?>
